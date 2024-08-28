af529ac231460e01f709aa0c41bcfd3b57f8adfd
kingsleynwoye <kingsleynwoye001@gmail.com>
2024-08-28 20:17:42 +0100

    Update codebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,10 @@ export default function Home() {
     <>
       <Head>
         <title>URECRUITED - Ace your interviews</title>
-        <meta name="description" content="" />
+        <meta
+          name="description"
+          content="Master your next job interview with URECRUITED! Train skills, get tailored advice, and receive real-time feedback to ace every question and land your dream job!"
+        />
         <meta name="viewport" content="width=deviceWidth, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -17,7 +20,12 @@ export default function Home() {
         {/* Banner */}
         <section
           className="flex min-h-screen flex-col items-center justify-center bg-cover bg-center text-white"
-          style={{ backgroundImage: "url(/background.png)" }}
+          style={{
+            backgroundImage: "url(/background.png)",
+            backgroundColor: "rgb(10, 14, 18)",
+            filter: "brightness(0.8)",
+            transition: "filter 0.3s ease 0s",
+          }}
         >
           <Image
             src="/brand.png"
